0ee5b2169efcaefd9aac9f705e85a922ad5bb90f
arip997 <arindam.panda@zineone.com>
2023-03-29 22:10:59 +0530

    added navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,39 +7,44 @@ import { links } from '../data/dummy'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-  const { activeMenu,setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
   const normalLink = `flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2 
                       text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray`;
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  }
 
 
   return (
     <div className='h-screen overflow-auto md:overflow-auto'>
       {activeMenu ? (<>
         <div className="flex justify-between items-center">
-          <Link to='/' onClick={() => setActiveMenu(false)} className='items-center gap-3 ml-4 mt-4 
+          <Link to='/' onClick={handleCloseSidebar} className='items-center gap-3 ml-4 mt-4 
           flex text-xl font-extrabold tracking-tight dart:text-white text-slate-900'>
             <SiShopware /> <span>Shoppy</span>
           </Link>
           <TooltipComponent content="Menu" position="BottomCenter">
-            <button type="button" onClick={() => {
+            {screenSize <= 900 && <button type="button" onClick={() => {
               setActiveMenu((prev) => !prev)
             }} className='text-xl p-4 hover:bg-light-gray
             mt-4'>
               <MdOutlineCancel />
-            </button>
+            </button>}
           </TooltipComponent>
         </div>
         <div className="mt-10">
           {links.map((item) => (
             <div key={item.title} className="">
               <p className='text-gray-400 mt-4 uppercase m-4'>{item.title}</p>
-              {item.links.map((link)=>(
+              {item.links.map((link) => (
                 <NavLink
-                to={`/${link.name}`}
-                key={link.name}
-                onClick={()=>{}}
-                className={({isActive})=> isActive ? activeLink : normalLink}
+                  to={`/${link.name}`}
+                  key={link.name}
+                  onClick={handleCloseSidebar}
+                  className={({ isActive }) => isActive ? activeLink : normalLink}
                 >
                   {link.icon}
                   <span className='capitalize'>
